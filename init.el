e456c3df889cbab8256955ac81fbe7ec5cc63190
mn <mn@linux-239s.site>
2014-01-30 20:58:30 +0100

    init changed

--- init.el
+++ init.el
@@ -27,3 +27,26 @@
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
+(starter-kit-load "misc-recommended")
+(starter-kit-load "js")
+
+(defvar my-packages '(                                       
+                      clojure-mode
+                      clojure-test-mode
+                      cider))
+
+(dolist (p my-packages)
+( when (not (package-installed-p p ))
+(package-install p)))
+
+(defun cider-eval-expression-at-point-in-repl ()
+  (interactive)
+  (let ((form (cider-sexp-at-point)))
+    ;; Strip excess whitespace
+    (while (string-match "\\`\s+\\|\n+\\'" form)
+      (setq form (replace-match "" t t form)))
+    (set-buffer (cider-find-or-create-repl-buffer))
+    (goto-char (point-max))
+    (insert form)
+    (cider-repl-return)))
+(global-set-key (kbd "C-`") 'cider-eval-expression-at-point-in-repl)
